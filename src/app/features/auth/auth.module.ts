a29c88b72b2528132761cc7d6ccdc71c8d94969d
triettran <tranvuminhtriet@gmail.com>
2022-10-05 10:52:41 +0700

    Update Login-page UI

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'app/shared/shared.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormPatchModule } from 'ng-zorro-antd/core/form';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 @NgModule({
   declarations: [
     LoginPageComponent,
     LoginFormComponent
   ],
-  imports: [ReactiveFormsModule, SharedModule, AuthRoutingModule],
+  imports: [
+    ReactiveFormsModule,
+    SharedModule,
+    AuthRoutingModule,
+    NzFormPatchModule,
+    NzInputModule,
+    NzButtonModule,
+    NzFormModule
+  ],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
+  isLoading = false;
+  loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required]),
+    })
+  }
+
+  // TO-DO: calling api
+  onSubmit() {
+    this.isLoading = true;
+    setTimeout(() => (this.isLoading = false), 1000)
+    console.log(this.loginForm);
   }
 
 }
