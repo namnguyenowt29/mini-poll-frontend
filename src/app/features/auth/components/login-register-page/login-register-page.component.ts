fbe74aefb8133f3055489973ac86a72e61b0a18c
triettran <tranvuminhtriet@gmail.com>
2022-10-05 15:16:59 +0700

    Bugfix: subcribe page-title login & register when changing routeLink

--- src/app/features/auth/components/login-register-page/login-register-page.component.ts
+++ src/app/features/auth/components/login-register-page/login-register-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-register-page',
@@ -7,13 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-register-page.component.scss']
 })
 export class LoginRegisterPageComponent implements OnInit {
+  urlSubscription: Subscription;
   pageTitle: string;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    if (this.router.url === '/login') this.pageTitle = "LOG IN"
-    if (this.router.url === '/register') this.pageTitle = "REGISTER"
+    this.pageTitle = this.getPageTitle(this.router.url)
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
+      const currentURL = (event as NavigationEnd).url
+      this.pageTitle = this.getPageTitle(currentURL)
+    });
+  }
+
+  getPageTitle(url: string) {
+    if (url === '/register') return "REGISTER"
+    return "LOG IN"
   }
 
 }
