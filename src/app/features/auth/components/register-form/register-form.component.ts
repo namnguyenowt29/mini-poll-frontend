fe1090828f4a34f8ea7df42b228f7573d14e4a36
triettran <tranvuminhtriet@gmail.com>
2022-10-10 10:16:24 +0700

    Initialize Form in constructor

--- src/app/features/auth/components/register-form/register-form.component.ts
+++ src/app/features/auth/components/register-form/register-form.component.ts
@@ -9,16 +9,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class RegisterFormComponent implements OnInit {
 
   isLoading = false;
-  registerForm !: FormGroup;
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
     this.registerForm = new FormGroup({
       'name': new FormControl(null, [Validators.required]),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required]),
     })
+  }
+
+  ngOnInit(): void {
   }
 
   // TO-DO: calling api
